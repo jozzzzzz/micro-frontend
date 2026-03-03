58b76db7389a4c87518ff678e6a31022c4398fde
jojo <jonathan.hayot@efrei.net>
2026-03-03 14:46:37 +0100

    first commit

---
+++ mfe-header/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import eventBus from 'shared/eventBus';
+import './Navbar.css';
+
+function Navbar() {
+  const [notifications, setNotifications] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = eventBus.on('game:joined', (data) => {
+      console.log('[EventBus] game:joined', data);
+      setNotifications(prev => prev + 1);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <span className="logo">PixelArena</span>
+        <span className="mfe-badge">MFE</span>
+      </div>
+
+      <div className="navbar-menu">
+        <button className="nav-button">Lobby</button>
+        <button className="nav-button">Scores</button>
+      </div>
+
+      <div className="navbar-user">
+        <span className="username">Joueur_42</span>
+        <button className="nav-button notification-btn">
+          {notifications > 0 && <span className="badge">{notifications}</span>}
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- mfe-header/webpack.config.js
+++ mfe-header/webpack.config.js
@@ -1,17 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
+    publicPath: 'http://localhost:3001/',
     clean: true,
   },
   devServer: {
-    port: 3000,
+    port: 3001,
     hot: true,
-    open: true,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   module: {
     rules: [
@@ -33,8 +38,22 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      shared: path.resolve(__dirname, '../shared'),
+    },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfeHeader',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Navbar': './src/components/Navbar',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^18.2.0' },
+        'react-dom': { singleton: true, requiredVersion: '^18.2.0' },
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
